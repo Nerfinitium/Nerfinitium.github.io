f56afa855eeecea58abd0a64dc25883eb2518419
Arch64 <buldukemir1@gmail.com>
2023-11-03 22:36:32 +0300

    test

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
+
+const Hero = () => {
+  return (
+   <section className="relative w-full h-screen mx-auto">
+    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff] " />
+             <div className="w-1 sm:h-80 h-40 violet-gradient" />
+             </div>
+             <div>
+              <h1 className={`${styles.heroHeadText} Text-white`}>Hi, I'm <span className="text-[#915eff]">Arch64</span></h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop in back end and front end im intrested in <br className="sm:block hidden"/>linux, app development, game development, web development
+              and much more.</p>
+             </div>
+         
+        </div>
+        <ComputersCanvas />
+    </section>
+  )
+}
+
+export default Hero
